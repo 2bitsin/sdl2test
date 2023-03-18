372fc9b9af8e428335131784373253466e0358d4
Aleksandr Ševčenko <aleksandr.sevcenko@tesonet.com>
2023-03-19 01:03:23 +0200

    ...

--- source/main.c
+++ source/main.c
@@ -9,15 +9,18 @@ int main(int argc,char** argv)
   SDL_Window* window = NULL;
   SDL_Event event;
 
-  SDL_Init(SDL_INIT_EVERYTHING);
-  atexit(&SDL_Quit);
-
   SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    return -1;
+
+  atexit(&SDL_Quit);
 
   window = SDL_CreateWindow("Hello !", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
   if (!window) {
     return -1;
   }
+  
   for (;;)
   {
     if (SDL_PollEvent(&event))
